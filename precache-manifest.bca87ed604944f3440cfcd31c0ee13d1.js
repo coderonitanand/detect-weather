47ee26ed7b95f5b3ebdbbac9ff5270ad2a7fa485
coderonitanand <62762338+coderonitanand@users.noreply.github.com>
2020-04-25 23:23:37 +0530

    Updates

---
+++ precache-manifest.bca87ed604944f3440cfcd31c0ee13d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06652b3b7e1b07674635f1a39dbe7d93",
+    "url": "/detect-weather/index.html"
+  },
+  {
+    "revision": "1a3e8707d24af5fab406",
+    "url": "/detect-weather/static/css/main.38f0804c.chunk.css"
+  },
+  {
+    "revision": "892349c35add50cabb55",
+    "url": "/detect-weather/static/js/2.dc11817f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/detect-weather/static/js/2.dc11817f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a3e8707d24af5fab406",
+    "url": "/detect-weather/static/js/main.034ede10.chunk.js"
+  },
+  {
+    "revision": "85d259e07c938d5b2068",
+    "url": "/detect-weather/static/js/runtime-main.112e3819.js"
+  }
+]);
